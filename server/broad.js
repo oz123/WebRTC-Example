e04c498d22ea7958d4cd02708c5d9cfa4b12fb24
Oz N Tiram <oz.tiram@gmail.com>
2023-05-04 06:57:58 +0200

    Add broadcasting websocket
    This is split out rom server.js

---
+++ server/broad.js
@@ -0,0 +1,46 @@
+const HTTPS_PORT = 18443;
+
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.Server;
+
+// Yes, TLS is required
+const serverConfig = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+};
+
+// ----------------------------------------------------------------------------------------
+
+// Create a server for the client html page
+const handleRequest = function(request, response) {
+  // Render the single client html file for any request the HTTP server receives
+  console.log('request received: ' + request.url);
+};
+
+const httpsServer = https.createServer(serverConfig, handleRequest);
+httpsServer.listen(HTTPS_PORT, '0.0.0.0');
+
+// ----------------------------------------------------------------------------------------
+
+// Create a server for handling websocket calls
+const wss = new WebSocketServer({server: httpsServer});
+
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    // Broadcast any received message to all clients
+    console.log('received: %s', message);
+    wss.broadcast(message);
+  });
+});
+
+wss.broadcast = function(data) {
+  this.clients.forEach(function(client) {
+    if(client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+console.log('Server running. websocket on wss://0.0.0.0:' + HTTPS_PORT);
